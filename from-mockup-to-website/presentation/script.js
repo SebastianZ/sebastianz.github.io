c12d619b274a8260ae17cab472a283f0e26875db
Sebastian Zartner <sebastianzartner@gmail.com>
2025-05-22 11:45:48 +0200

    Added presentation to "From mockup to website" project

---
+++ from-mockup-to-website/presentation/script.js
@@ -0,0 +1,8 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+Reveal.initialize({
+	plugins: [ RevealHighlight ],
+	disableLayout: false,
+	view: 'scroll',
+});
